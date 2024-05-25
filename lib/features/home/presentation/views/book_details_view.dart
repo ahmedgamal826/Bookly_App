a21af32b9015dc91337391af455b9a909ea5993e
Ahmed Elnemr <aelnemr711@gmail.com>
2024-05-25 22:54:00 +0300

    Finish Bookly App

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/core/models/book_model/book_model.dart';
-import 'package:bookly_app/features/home/presentation/manager/search_books_cubit/similar_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
